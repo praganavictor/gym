64afdac37fd90e8a18f4b6c54926cd515ea6906c
Victor Pragana <victorpragana@tickpay.com>
2024-11-20 23:07:32 -0300

    applying invertion dependency and repository pattern

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send()
+  }
+  return reply.status(201).send()
+}
