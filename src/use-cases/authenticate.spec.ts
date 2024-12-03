642883c2e2c83e9c428327b73a0e86ad0fc1136b
Victor Pragana <victorpragana@tickpay.com>
2024-12-03 18:16:55 -0300

    adding authentication and applying factory pattern

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,54 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-respository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { AuthenticateUseCase } from './authenticate'
+import { InvalidCredencialsError } from './errors/invalid-credentials-error'
+import { hash } from 'bcryptjs'
+
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
+
+describe('Register User Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(usersRepository)
+  })
+
+  it('should be able to authenticate', async () => {
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredencialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredencialsError)
+  })
+})
--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,32 +1,56 @@
-import { compare } from 'bcryptjs'
-import { expect, describe, it } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-respository'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
-describe('Register Use Case', () => {
-  it('should hash user password upon registration', async () => {
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail(email) {
-        return null
-      },
-      async create(data) {
-        return {
-          id: 'user-1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
-    })
-    const { user } = await registerUseCase.execute({
+let usersRepository: InMemoryUsersRepository
+let sut: RegisterUseCase
+
+describe('Register User Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new RegisterUseCase(usersRepository)
+  })
+
+  it('should to register', async () => {
+    const { user } = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
     })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upon registration', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
     const isPasswordCorrectlyHashed = await compare(
       '123456',
       user.password_hash,
     )
+
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const email = 'johndoe@example.com'
+    await sut.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+    await expect(() =>
+      sut.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
