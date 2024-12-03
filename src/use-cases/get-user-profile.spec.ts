35cfd9633b7f2bb54e742e09475c0c0bf0bdde4b
Victor Pragana <victorpragana@tickpay.com>
2024-12-03 18:51:12 -0300

    adding check in use-cases

--- src/use-cases/get-user-profile.spec.ts
+++ src/use-cases/get-user-profile.spec.ts
@@ -22,7 +22,7 @@ describe('Get User Profile Use Case', () => {
     expect(user.name).toEqual('John Doe')
   })
   it('should not be able to get user profile with wrong id', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         userId: 'non-existing-id',
       }),
